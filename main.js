131a73bf113cbe0a07bf05940b95dce3da6a4656
Karim Korgoloev @Mac <korgoloevk@gmail.com>
2022-07-22 19:05:16 +0300

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-function setQuestion() {
+let currentQuestion = 0;
+let totalCount = 0;
+const liElem = document.querySelectorAll("li");
+
+function setQuestion(obj) {
   const question = document.querySelector(".question");
-  const liElem = document.querySelectorAll("li");
 
   liElem.forEach((elem, i) => {
-    elem.textContent = questions[0].answers[i][0];
-    elem.setAttribute("score", questions[0].answers[i][1]);
+    elem.textContent = obj[currentQuestion].answers[i][0];
+    elem.setAttribute("score", obj[currentQuestion].answers[i][1]);
   });
 
-  question.textContent = questions[0].question;
+  question.textContent = obj[currentQuestion].question;
 }
 
-setQuestion();
+function setAnswer() {
+  currentQuestion++;
+  totalCount += +this.getAttribute("score");
+  setQuestion(questions);
+}
+
+function setNextCount(){
+    
+}
+
+liElem.forEach((elem) => {
+  elem.addEventListener("click", setAnswer);
+});
+
+setQuestion(questions);
