44e0adaa18dadd1e243ac1771a4e25877527c244
Karim Korgoloev @Mac <korgoloevk@gmail.com>
2022-07-25 10:45:19 +0300

    work

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 let currentQuestion = 0;
 let totalCount = 0;
 const liElem = document.querySelectorAll("li");
+const question = document.querySelector(".question");
+const counts = document.querySelectorAll(".count");
+const questionLength = Math.max(...Object.keys(questions));
 
-function setQuestion(obj) {
-  const question = document.querySelector(".question");
-
+function setQuestion(obj, index) {
   liElem.forEach((elem, i) => {
-    elem.textContent = obj[currentQuestion].answers[i][0];
-    elem.setAttribute("score", obj[currentQuestion].answers[i][1]);
+    elem.textContent = obj[index].answers[i][0];
+    elem.setAttribute("score", obj[index].answers[i][1]);
   });
-
-  question.textContent = obj[currentQuestion].question;
+  question.textContent = obj[index].question;
+  counts[index].setAttribute("data-visibility", "visible");
 }
 
 function setAnswer() {
+  if (currentQuestion >= +questionLength) return false;
   currentQuestion++;
   totalCount += +this.getAttribute("score");
-  setQuestion(questions);
+  setQuestion(questions, currentQuestion);
 }
 
-function setNextCount(){
-    
-}
+function setNextCount() {}
 
 liElem.forEach((elem) => {
   elem.addEventListener("click", setAnswer);
 });
 
-setQuestion(questions);
+setQuestion(questions, currentQuestion);
