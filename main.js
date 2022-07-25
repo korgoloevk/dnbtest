b0f14a3a022b2872467b08470b8d6f088b30c053
Karim Korgoloev @Mac <korgoloevk@gmail.com>
2022-07-25 13:07:29 +0300

    work

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const liElem = document.querySelectorAll("li");
 const question = document.querySelector(".question");
 const counts = document.querySelectorAll(".count");
 const questionLength = Math.max(...Object.keys(questions));
+const test = document.querySelector(".test");
 
 function setQuestion(obj, index) {
   liElem.forEach((elem, i) => {
@@ -15,13 +16,44 @@ function setQuestion(obj, index) {
 }
 
 function setAnswer() {
-  if (currentQuestion >= +questionLength) return false;
+  if (currentQuestion >= +questionLength) {
+    setResult();
+    return false;
+  }
   currentQuestion++;
   totalCount += +this.getAttribute("score");
   setQuestion(questions, currentQuestion);
 }
 
-function setNextCount() {}
+function setResult() {
+  const resultText = document.createElement("div");
+  resultText.classList.add("qst");
+  let resultImage = document.createElement("img");
+
+  if (totalCount < 8) {
+    resultText.textContent = images.vldl.text;
+    resultImage = images.vldl.img;
+  } else if (totalCount < 20) {
+    resultText.textContent = images.oxi.text;
+    resultImage = images.oxi.img;
+  } else if (totalCount < 54) {
+    resultText.textContent = images.fixik.text;
+    resultImage = images.fixik.img;
+  } else if (totalCount < 72) {
+    resultText.textContent = images.romeo.text;
+    resultImage = images.romeo.img;
+  } else if (totalCount < 90) {
+    resultText.textContent = images.ded.text;
+    resultImage = images.ded.img;
+  } else if (totalCount < 108) {
+    resultText.textContent = images.brsrk.text;
+    resultImage = images.brsrk.img;
+  }
+  resultImage.classList.add("resultImage");
+  test.innerHTML = "";
+  test.append(resultText);
+  test.append(resultImage);
+}
 
 liElem.forEach((elem) => {
   elem.addEventListener("click", setAnswer);
